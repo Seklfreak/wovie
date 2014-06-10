f73c8f8074a6f88fffa3f18452e0e69e41860d4d
Sebastian Winkler <sekl@slmn.de>
2014-06-10 11:55:55 +0200

    better webhook

--- src/SLMN/Wovie/MainBundle/Controller/WebhookController.php
+++ src/SLMN/Wovie/MainBundle/Controller/WebhookController.php
@@ -10,26 +10,33 @@ class WebhookController extends Controller
 {
     public function stripeAction()
     {
+        $response = new Response();
         $input = @file_get_contents('php://input');
 
-        $event_json = json_decode($input, true);
-        $event = \Stripe_Event::retrieve($event_json['id']);
+        $event_json = json_decode($input);
+        $event = \Stripe_Event::retrieve($event_json->id);
 
         switch ($event['type'])
         {
             case 'invoice.created':
                 $stripeCustomersRepo = $this->getDoctrine()->getRepository('SLMNWovieMainBundle:StripeCustomer');
-                $customer = $stripeCustomersRepo->findOneByCustomerId($event['customer']);
-                $customer->setPaidUntil(new \DateTime($event['lines'][0]['data'][0]['period'][0]['end']));
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($customer);
-                $em->flush();
+                $customer = $stripeCustomersRepo->findOneByCustomerId($event->customer);
+                if ($customer)
+                {
+                    $customer->setPaidUntil(new \DateTime($event->lines[0]->data[0]->period[0]->end));
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($customer);
+                    $em->flush();
+                }
+                else
+                {
+                    $response->setContent('Customer not found!');
+                    $response->setStatusCode(500);
+                }
                 break;
             default:
                 break;
         }
-
-        $response = new Response();
         return $response;
     }
 } 
