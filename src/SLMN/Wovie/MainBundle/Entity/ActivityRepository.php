3bb39b5a8a93764ec8c7242b9641cd946781e134
Sebastian Winkler <sekl@slmn.de>
2014-07-20 14:45:17 +0200

    better activity pagination

--- src/SLMN/Wovie/MainBundle/Entity/ActivityRepository.php
+++ src/SLMN/Wovie/MainBundle/Entity/ActivityRepository.php
@@ -18,29 +18,19 @@ class ActivityRepository extends EntityRepository
         }
         $users[] = $user;
 
-        $dateStart = new \DateTime();
-        $dateStart->modify('-'.(24*intval($offset)).' hour');
-        $dateTo = new \DateTime();
-        $dateTo->modify('-'.(24*(intval($offset)+1)).' hour');
         $query = $this->createQueryBuilder('activity')
             ->where('activity.user IN (:users)')
-            ->andWhere('activity.time < :dateStart')
-            ->andWhere('activity.time > :dateTo')
             ->orWhere('activity.value = :me')
-            ->andWhere('activity.time < :dateStart')
-            ->andWhere('activity.time > :dateTo')
             ->setParameters(array(
                 'me' => serialize($user->getId()),
-                'users' => $users,
-                'dateStart' => $dateStart,
-                'dateTo' => $dateTo
+                'users' => $users
             ))
             ->orderBy('activity.time', 'DESC')
+            ->setMaxResults(10)
+            ->setFirstResult($offset*10)
             ->getQuery();
 
         $result = $query->getResult();
         return $result;
-
-        return $activities;
     }
 }
