855eb3b698be03c0338a35b89a613d14c33ce83f
Sebastian Winkler <sekl@slmn.de>
2014-08-23 15:36:57 +0200

    Added simple admin functions

--- src/SLMN/Wovie/MainBundle/Controller/AdminController.php
+++ src/SLMN/Wovie/MainBundle/Controller/AdminController.php
@@ -9,7 +9,54 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
-    public function emailPreviewAction($template, $inlined) {
+    public function dashboardAction()
+    {
+        $usersRepo = $this->getDoctrine()
+            ->getRepository('SeklMainUserBundle:User');
+        $mediasRepo = $this->getDoctrine()
+            ->getRepository('SLMNWovieMainBundle:Media');
+        $viewsRepo = $this->getDoctrine()
+            ->getRepository('SLMNWovieMainBundle:View');
+
+        $totalUsers = $usersRepo->createQueryBuilder('object')
+                ->select('count(object.id)')
+                ->getQuery()->getSingleScalarResult();
+        $totalMedias = $mediasRepo->createQueryBuilder('object')
+                ->select('count(object.id)')
+                ->getQuery()->getSingleScalarResult();
+        $totalViews = $viewsRepo->createQueryBuilder('object')
+                ->select('count(object.id)')
+                ->getQuery()->getSingleScalarResult();
+
+        return $this->render(
+            'SLMNWovieMainBundle:html/user/admin:tab-dashboard.html.twig',
+            array(
+                'totalUsers' => $totalUsers,
+                'totalMedias' => $totalMedias,
+                'totalViews' => $totalViews
+                )
+            );
+    }
+
+    public function broadcastsAction()
+    {
+        return $this->render('SLMNWovieMainBundle:html/user/admin:tab-broadcasts.html.twig');
+    }
+
+    public function emailTemplatesAction()
+    {
+        // get template files
+        $bundles = $this->get('kernel')->getBundles();
+        $templates = scandir($bundles['SLMNWovieMainBundle']->getPath().'/Resources/views/email/');
+
+        return $this->render(
+            'SLMNWovieMainBundle:html/user/admin:tab-emailTemplates.html.twig',
+            array('templates' => $templates)
+            );
+    }
+
+    public function emailPreviewAction($template, $inlined)
+    {
         if ($inlined == false) {
             return $this->render('SLMNWovieMainBundle:email:'.$template.'.html.twig', array(
             ));
